c3b25bf384cba6454d93bc8347f6fb1e54f2c212
Robert Bouschery <robert.bouschery@gmail.com>
2019-05-24 23:30:17 +0200

    Made map markers better understandable for screen readers

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -167,7 +167,7 @@ class DBHelper {
     // https://leafletjs.com/reference-1.3.0.html#marker
     const marker = new L.marker([restaurant.latlng.lat, restaurant.latlng.lng],
       {title: restaurant.name,
-      alt: restaurant.name,
+      alt: restaurant.name + 'Map Marker' ,
       url: DBHelper.urlForRestaurant(restaurant)
       })
       marker.addTo(newMap);
