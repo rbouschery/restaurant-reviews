d7e44e4d7b63b0a7bfd758f5cbb563e424955d92
Robert Bouschery <robert.bouschery@gmail.com>
2019-05-25 15:19:09 +0200

    added image caching to the service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var myCache = 'rr-cache-v5';
+var myCache = 'rr-cache-v6';
 let urlsToCache = [
   '/',
   '/restaurant.html?id=1',
@@ -13,7 +13,17 @@ let urlsToCache = [
   '/restaurant.html?id=10',
   '/css/styles.css',
   '/js/main.js',
-  '/js/restaurant_info.js'
+  '/js/restaurant_info.js',
+  'img/1.jpg',
+  'img/2.jpg',
+  'img/3.jpg',
+  'img/4.jpg',
+  'img/5.jpg',
+  'img/6.jpg',
+  'img/7.jpg',
+  'img/8.jpg',
+  'img/9.jpg',
+  'img/10.jpg'
 ];
 
 self.addEventListener('install', function(event) {
